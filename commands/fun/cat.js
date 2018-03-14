ab1f406c17810a0335b819994a415158009c9bf8
DerpyTheCoder <DerpyTheCoder@users.noreply.github.com>
2018-03-14 23:59:14 +0100

    Added first cats
    Meeeow!

--- commands/fun/cat.js
+++ commands/fun/cat.js
@@ -16,8 +16,18 @@ module.exports = class Meow extends Base {
         if (message.channel.name === "dfchat") return super.error("You can't use that here!");
 
         const meowStorage = [
-            "Cat 1.",
-            "Cat 2."
+            "http://random.cat/i/068_-_6vuLGR1.gif",
+            "http://random.cat/i/uUi0a.jpg",
+            "http://random.cat/i/mFC8oVK.jpg",
+            "http://random.cat/i/YTgRq.jpg",
+            "http://random.cat/i/o7pjQLT.jpg",
+            "http://random.cat/i/iVpaM.jpg",
+            "http://random.cat/i/249053_10151615620946211_1427883182_n.jpg",
+            "http://random.cat/i/Nmn7a.jpg",
+            "http://random.cat/i/VjIboF2.jpg",
+            "http://random.cat/i/img_20161017_122411.jpg",
+            "http://random.cat/i/GQXOl.png",
+            "http://random.cat/i/t3nMb.jpg"
         ];
 
         // Send a response
