c323cf1dfb522a25eaa0d03b38b7d6ab3e973aca
DerpyTheCoder <DerpyTheCoder@users.noreply.github.com>
2018-03-14 23:48:14 +0100

    Testinf cats
    Meow

---
+++ commands/fun/cat.js
@@ -0,0 +1,26 @@
+const Base = require("../../base/Command.js");
+
+module.exports = class Meow extends Base {
+    constructor(client) {
+        super(client, {
+            name: "cat",
+            description: "Sends a random cat image.",
+            usage: "<cat>",
+            category: "fun",
+            permLevel: 0 
+        });
+    }
+
+    run(message, args) {
+        // Ignore if sent in dfchat
+        if (message.channel.name === "dfchat") return super.error("You can't use that here!");
+
+        const responses = [
+            "Cat 1.",
+            "Cat 2."
+        ];
+
+        // Send a response
+        message.channel.send(`[**Cat**] ${responses[Math.floor(Math.random() * responses.length)]}`);
+    }
+};
