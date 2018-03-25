a49e18af0b7451e2ebc90bac345b4e51203a0530
DerpyTheCoder <DerpyTheCoder@users.noreply.github.com>
2018-03-25 01:12:45 +0100

    add more responses

--- commands/fun/idea.js
+++ commands/fun/idea.js
@@ -24,7 +24,17 @@ module.exports = class Meow extends Base {
                 "A Horror Game",
                 "A Magic Game",
                 "A KitPvP",
-                "A Trivia Game"
+                "A Trivia Game",
+                "A Exploration Game",
+                "A Adventure Game",
+                "A Real time Strategy Game",
+                "A grindy escapist",
+                "A grindy survival",
+                "A Stratgy",
+                "A Board Game",
+                "A Mob Arena",
+                "A Dropper Game",
+                "A Spleef Game"
             ]
         });
         
@@ -36,18 +46,48 @@ module.exports = class Meow extends Base {
                 "where you collect items",
                 "where you shoot lasers",
                 "where you mirror movement",
-                "where you rebuild structures"
+                "where you rebuild structures",
+                "where you farm points",
+                "where you kill players",
+                "where you battle mobs",
+                "where you grind gold",
+                "where you create a farm",
+                "where you build a fort",
+                "where you destroy a fort",
+                "where you report a player",
+                "where you break plot rules",
+                "where you die",
+                "where you jump",
+                "where you fly",
+                "where you build",
+                "where you swim",
+                "where you cant sprint",
+                "where you play minigames",
+                "where you battle Jeremaster",
+                "where you battle a mob",
+                "where you write fanfictions",
+                "where you break everything",
+                "where you get demonetized",
+                "where you kill animals",
+                "where you kill mobs",
+                "where you code",
+                "where you drown",
+                "where you eat cake"
             ]
         });
         
         Object.defineProperty(this, "gameReward", {
             value: [
+                "until you die.",
+                "until time runs out.",
                 "to get points.",
                 "to get escapes.",
-                "to get win.",
-                "and kill everything.",
+                "to win.",
+                "to end the game.",
+                "to fail.",
+                "to kill everything.",
                 "and have to beat all levels",
-                "with friends"
+                "with friends."
             ]
         });
 
