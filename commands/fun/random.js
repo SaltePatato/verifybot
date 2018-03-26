6f1246d842e02bced1c2b0c4cd270c46f93aae17
LittleWhole <wholelittle@gmail.com>
2018-03-26 11:39:39 -0400

    Create random.js

---
+++ commands/fun/random.js
@@ -0,0 +1,26 @@
+const Base = require("../../base/Command.js");
+
+module.exports = class Random extends Base {
+    constructor(client) {
+        super(client, {
+            name: "random",
+            description: "Generates a random number.",
+            usage: "<min> <max>",
+            category: "fun",
+            permLevel: 0 
+        });
+    }
+
+    async run(message, args) {
+        // Ignore if sent in dfchat
+        if (message.channel.name === "dfchat") return super.error("You can't use that here!");
+        
+        let numberOne = args[1]
+        let numberTwo = args[2]
+        let rnd = Math.floor(Math.random() * numberTwo) + numberOne
+    }
+
+    pause() {
+        return new rnd(r => setTimeout(r, 5000));
+    }
+};
