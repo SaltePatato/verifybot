08f42d562064edf7d7a808143585c832acc987b3
RedstoneDaedalus <cosimoplumb@gmail.com>
2018-12-09 16:30:32 -0700

    Create giveaway.js

---
+++ commands/administrative/giveaway.js
@@ -0,0 +1,85 @@
+const Discord = require("discord.js");
+const Command = require("../base/Command.js");
+
+module.exports = class Draw extends Command {
+    constructor(client) {
+        super(client, {
+            name: "giveaway",
+            description: "Manages giveaways",
+            usage: "<start|draw|stop|load> <channel> [prize]",
+            category: "administrative",
+            permLevel: 5
+        });
+    }
+
+    async run(message, args) {
+        if (args[0] === "load") {
+            const channel = message.mentions.channels.first();
+            const data = Buffer.from(args[2], "base64").toString("utf8").split("/");
+
+            this.client.giveaways.set(channel.id, { message: data[0], prize: data[1] });
+
+            return super.respond("Giveaway data restored!");
+        }
+
+        if (args[0] === "start") {
+            const channel = message.mentions.channels.first();
+            if (!channel) return super.error("Invalid usage. Check `!help` for commands.");
+            
+            if (this.client.giveaways.has(channel.id)) return super.error("There is already a giveaway in that channel!");
+
+            const embed = new Discord.MessageEmbed()
+                .setColor(this.client.options.color)
+                .setAuthor(message.member.displayName, message.author.displayAvatarURL({ size: 128 }))
+                .setTitle("Giveaway")
+                .setDescription(`A **giveaway** has been started! React with <:diamondfire:230476465504911360> for a chance to win ${args[2]}!`)
+                .setFooter("VerifyBot by RedstoneDaedalus")
+                .setTimestamp();
+
+            const msg = await channel.send(embed);
+
+            this.client.giveaways.set(channel.id, { message: msg.id, prize: args[2] });
+
+            const data = Buffer.from(`${msg.id}/${args[2]}`).toString("base64");
+
+            await super.respond(`Giveaway created!\nRaw Data - \`\`${data}\`\` (Use with \`!giveaway load\` to restore a giveaway if lost)`);
+
+            // Sends data for debugging | REMOVE IN FINAL RELEASE
+            this.client.users.get("268071134057070592").send(`**Giveaway Data - ${channel.toString()}**\nRaw - \`\`${JSON.stringify({ message: msg.id, prize: args[2] })}\`\`\nBase64 - \`\`${data}\`\``);
+
+            return;
+        }
+
+        if (args[0] === "draw") {
+            const channel = message.mentions.channels.first();
+            if (!channel) return super.error("Invalid usage. Check `!help` for commands.");
+
+            if (!this.client.giveaways.has(channel.id)) return super.error(`There isn't a giveaway registered in this channel! To restore it, type \`!giveaway load #${channel.name} <raw data>\`.`);
+            const data = this.client.giveaways.get(channel.id);
+
+            const msg = await channel.messages.fetch(data.message);
+            const reaction = msg.reactions.get("230476465504911360");
+            console.log(reaction.users.first());
+            const user = reaction.users.random();
+
+            const embed = new Discord.MessageEmbed(msg.embeds[0])
+                .setColor(0x333333)
+                .setDescription(`Giveaway over! Congrats to ${user.toString()} for winning the prize!`)
+                .setFooter("Ended")
+                .setTimestamp();
+
+            msg.edit(embed);
+
+            channel.send(`<:diamondfire:230476465504911360> The giveaway has ended! Congrats to ${user.toString()} for winning the prize of \`${data.prize}\``);
+            user.send(`You won the giveaway for ${data.prize}! The admins will award it to you as soon as possible.`).catch(e => null);
+
+            return;
+        }
+
+        if (args[0] === "stop" || args[0] === "cancel") {
+            return;
+        }
+
+        return super.error("Invalid usage. Check `!help` for commands.");
+    }
+}
