761a1218147f5b9084356d8544bb08d2d882c601
Tomoli75 <33159744+Tomoli75@users.noreply.github.com>
2018-12-28 08:42:14 +0000

    Update cat.js

--- commands/fun/cat.js
+++ commands/fun/cat.js
@@ -1,47 +1,37 @@
+const { get } = require("snekfetch");
 const Base = require("../../base/Command.js");
 
-module.exports = class Meow extends Base {
+module.exports = class Dog extends Base {
     constructor(client) {
         super(client, {
             name: "cat",
-            description: "Sends a random cat image.",
+            description: "Shows a random cat.",
             usage: "",
             category: "fun",
             permLevel: 0,
             cooldown: 60000
         });
-var https = require('https');
-
-//Tomoli's fancy cat command
-//Runs on external server
-var options = {
-  host: 'verifybot.tomoli.cf',
-  path: '/cat.php'
-};
-
-callback = function(response) {
-  var str = '';
-
-  //another chunk of data has been recieved, so append it to `str`
-  response.on('data', function (chunk) {
-    str += chunk;
-  });
-
-  //the whole response has been recieved, so we just print it out here
-  response.on('end', function () {
-    this.chosen = str;
-  });
-}
-
-https.request(options, callback).end();
     }
 
-    run(message) {
+    async run(message) {
         // Ignore if sent in dfchat
         if (message.channel.name === "dfchat") return super.error("You can't use that here!");
 
-        // Send a response
-        message.channel.send(`[**Cat**] ${this.chosen}`);
-        // New cat is received when command run
+        // Send a dog
+        await message.channel.send(this.cache.shift());
+        // Add a new dog to cache
+        this.fillCache(1);
+    }
+
+    async fillCache(amount = 5) {
+        for (let i = 0; i < amount; i++) {
+            const { text } = await get("https://verifybot.tomoli.cf/cat.php");
+            this.cache.push(`${text}`);
+        }
+    }
+
+    init() {
+        Object.defineProperty(this, "cache", { value: [] });
+        this.fillCache();
     }
 };
