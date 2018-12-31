0c9e2c21db6a6fbd4651eab39d9c734cb56bd35b
RedstoneDaedalus <cosimoplumb@gmail.com>
2018-12-31 12:31:23 -0700

    small fix

--- commands/fun/upload.js
+++ commands/fun/upload.js
@@ -16,11 +16,13 @@ module.exports = class Upload extends Base {
         // Get the attachment
         const attachment = message.attachments.first();
 
-      if(attachment) {
-          // Upload
-        get(`https://verifybot.tomoli.cf/addurl.php?link={attachment.url}`);
-        return super.respond("Your image has been uploaded! Your image will appear in !cat or !dog once our moderators have accepted it.");
-      }
-      return super.error("Please attach your dog or cat image to your message.");
+        if (attachment) {
+            // Upload
+            await get(`https://verifybot.tomoli.cf/addurl.php?link={attachment.url}`);
+            message.delete();
+            return super.respond("Your image has been uploaded! Your image will appear in !cat or !dog once our moderators have accepted it.");
+        }
+        message.delete();
+        return super.error("Please attach your dog or cat image to your message.");
     }
 };
