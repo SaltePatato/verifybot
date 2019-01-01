4a5100478e808361480c2ab20f1d8af233500e71
LittleWhole <wholelittle@gmail.com>
2019-01-01 20:13:53 +0800

    fixed some stuff

--- commands/fun/math.js
+++ commands/fun/math.js
@@ -32,7 +32,9 @@ module.exports = class Math extends Base {
             const operation = args[0]
             const one = parseInt(args[1]);
             const two = parseInt(args[2]);
-
+              
+            if (operation !== "floor" || operation !== "ceil" || operation !== "n-root" || operation !== "sec" || operation !== "csc" || operation != "cot") return message.channel.send({embed: mEmbed});
+                
             if (["exponent"].includes(operation) && isNaN(two)) return message.reply("This operation requires a second parameter.");
 
             const { pow, sqrt, floor, ceil, sin, cos, tan } = Math;
@@ -42,8 +44,9 @@ module.exports = class Math extends Base {
             else if (operation === "sec") output = 1 / cos(one * Math.PI / 180.0);
             else if (operation === "csc") output = 1 / sin(one * Math.PI / 180.0);
             else if (operation === "cot") output = 1 / tan(one * Math.PI / 180.0);
+              
+            
           }
-          super.respond("Your answer:");
           message.channel.send({embed: mEmbed})
         }
     }
