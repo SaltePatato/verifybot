fc778a3a6d5ee815be343e1dcdc6294d3e7f048a
RedstoneDaedalus <cosimoplumb@gmail.com>
2019-09-20 21:56:50 -0600

    Condensed presence code

--- events/ready.js
+++ events/ready.js
@@ -35,32 +35,12 @@ module.exports = class {
             // Fetch node data
             const { online, offline, list } = await fetchData(this.client);
             
-            if (nodesOnline.length > online.length) {
-                const crashed = nodesOnline.filter(n => !online.map(({ node }) => node).includes(n));
-                const message = `${crashed.length > 1 ? `Nodes ${crashed.slice(0, crashed.length - 1).join(", ")} and ${crashed.pop()}` : `Node ${crashed[0]}`} crashed! Check ${channel.toString()} for further information.`;
-
-                this.client.guild.members.filter(member => member.roles.exists("name", "Developer")).forEach(member => {
-                    member.send(message).catch(() => null);
-                });
-            }
-
-            if (offline.length >= 1) {
-                this.client.user.setPresence({
-                    activity: {
-                        name: offline.length > 1 ? `Nodes ${offline.map(({ node }) => node).offline.slice(0, offline.length - 1).join(", ")} and ${offline[offline.length - 1].node} offline` : `Node ${offline[0].node} offline`,
-                        type: 0
-                    },
-                    status: "dnd"
-                });
-            } else {
-                this.client.user.setPresence({
-                    activity: {
-                        name: `with ${list.reduce((out, data) => data.players + out, 0)} players`,
-                        type: 0
-                    },
-                    status: "online"
-                });
-            }
+            this.client.user.setPresence({
+                activity: {
+                    name: `for !help | ${list.reduec((out, data) => data.players + out, 0)} players`,
+                    type: "WATCHING"
+                }
+            });
 
         // Fetch 12:00 PM
         // const dt = new Date();
