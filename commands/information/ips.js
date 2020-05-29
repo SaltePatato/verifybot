9a556304eac9b0e9e7e98116083e5c4053ab9482
Daedalus <redstonedaedalus@gmail.com>
2020-05-29 13:57:49 -0600

    Update ips.js

--- commands/information/ips.js
+++ commands/information/ips.js
@@ -7,11 +7,12 @@ module.exports = class IP extends Command {
             description: "Server IP addresses.",
             category: "information",
             permLevel: 0,
-            clean: false
+            clean: false,
+            aliases: ["ip"]
         });
     }
 
     run(message) {
-         message.channel.send("**__DiamondFire IP Addresses__**\n\n`mcdiamondfire.com`\n\n`node1.mcdiamondfire.com`, `node2.mcdiamondfire.com`, `node3.mcdiamondfire.com`, `node4.mcdiamondfire.com`\n`beta.mcdiamondfire.com` (Overlord exclusive)");
+         message.channel.send("**__DiamondFire IP Addresses__**\n**Main IP:** `mcdiamondfire.com`\n\n**Node IPs:**\n`node1.mcdiamondfire.com`, `node2.mcdiamondfire.com`, `node3.mcdiamondfire.com`, `node4.mcdiamondfire.com`\n`beta.mcdiamondfire.com` (Overlord exclusive)");
     }
 };
